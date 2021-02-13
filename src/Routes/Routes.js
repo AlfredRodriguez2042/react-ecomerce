5b8ea75b9a73f4b8290dcbfdb866e963e19a7ff0
alfred Rodriguez G <alfred2042@hotmail.com>
2021-02-13 14:21:15 -0300

    add private route and admin route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,35 +1,80 @@
 import { lazy } from 'react'
 import { SingleProduct } from '../Components/Product'
+import PublicLayout from '../Layout/Public'
+import AdminLayout from '../Layout/Admin'
+import Page404 from '../Views/Page404'
 
 const Home = lazy(() => import('../Views/Home'))
 const Cart = lazy(() => import('../Views/Cart'))
 const SingUp = lazy(() => import('../Views/SignUp'))
 const Login = lazy(() => import('../Views/Login'))
+const Dashboard = lazy(() => import('../Views/Dashboard'))
 
-export const Routes = [
-  {
-    path: '/',
-    exact: true,
-    component: Home,
-  },
-  {
-    path: '/signup',
-    exact: true,
-    component: SingUp,
-  },
-  {
-    path: '/login',
-    exact: true,
-    component: Login,
-  },
-  {
-    path: '/app/cart/',
-    exact: true,
-    component: Cart,
-  },
-  {
-    path: '/product/:id',
-    exact: true,
-    component: SingleProduct,
-  },
-]
+const Public = {
+  path: '/',
+  name: 'home',
+  component: PublicLayout,
+  childRoutes: [
+    {
+      path: '/',
+      exact: true,
+      component: Home,
+    },
+    {
+      path: '/signup',
+      exact: true,
+      component: SingUp,
+    },
+    {
+      path: '/login',
+      exact: true,
+      component: Login,
+    },
+    {
+      path: '/app/cart/',
+      exact: true,
+      component: Cart,
+    },
+    {
+      path: '/product/:id',
+      exact: true,
+      component: SingleProduct,
+    },
+    {
+      path: '*',
+      component: Page404,
+    },
+  ],
+}
+
+const PrivateRoute = {
+  path: '/app',
+  name: 'home',
+  component: PublicLayout,
+  childRoutes: [
+    {
+      path: '/',
+      exact: true,
+      component: Home,
+    },
+    {
+      path: '/signup',
+      exact: true,
+      component: SingUp,
+    },
+  ],
+}
+const Admin = {
+  path: '/admin',
+  name: 'home',
+  component: AdminLayout,
+  childRoutes: [
+    {
+      path: '',
+
+      component: Dashboard,
+    },
+  ],
+}
+
+export const main = [Admin, Public, PrivateRoute]
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,20 +1,66 @@
 import React, { lazy, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { Routes } from './Routes'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import { main } from './Routes'
 
 const Page404 = lazy(() => import('../Views/Page404'))
 const AppRouter = () => {
+  const renderRoutes = (routes, contextPath) => {
+    const children = []
+    const role = 2
+    const isAuth = null
+
+    const renderRoute = (item, routeContextPath) => {
+      let newContextPath = item.path
+        ? `${routeContextPath}/${item.path}`
+        : routeContextPath
+      newContextPath = newContextPath.replace(/\/+/g, '/')
+      if (newContextPath.includes('admin') && role !== 1) {
+        item = {
+          ...item,
+          component: () => <Redirect to="/" />,
+          children: [],
+        }
+      }
+      if (newContextPath.includes('app') && isAuth !== 'isAuth') {
+        item = {
+          ...item,
+          component: () => <Redirect to="/" />,
+          children: [],
+        }
+      }
+      //   if (!item.component) return
+
+      if (item.childRoutes) {
+        const childRoutes = renderRoutes(item.childRoutes, newContextPath)
+        children.push(
+          <Route
+            key={newContextPath}
+            render={(props) => (
+              <item.component {...props}>{childRoutes}</item.component>
+            )}
+            path={newContextPath}
+          />
+        )
+        item.childRoutes.forEach((r) => renderRoute(r, newContextPath))
+      } else {
+        children.push(
+          <Route
+            key={newContextPath}
+            component={item.component}
+            path={newContextPath}
+            exact
+          />
+        )
+      }
+    }
+    routes.forEach((item) => renderRoute(item, contextPath))
+    return <Switch>{children}</Switch>
+  }
+  const Routes = renderRoutes(main, '/')
   return (
     <Suspense fallback={<h2>Loading...</h2>}>
       <Switch>
-        {Routes.map((route, i) => (
-          <Route
-            key={i}
-            path={route.path}
-            exact={route.exact}
-            component={route.component}
-          />
-        ))}
+        {Routes}
 
         <Route component={Page404} />
       </Switch>
